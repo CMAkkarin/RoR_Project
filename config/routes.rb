d8a5ceaff0ebf71915e372eeff3e2150f90d6dd8
CasperG <gwozdz.kacper@gmail.com>
2019-01-08 21:51:26 +0100

    #1 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'posts#index', as: 'home'
+  get 'about' => 'pages#about', as: 'about'
+
+  # maps HTTP verbs to controller actions
+  resources :posts 
 end
